5ccab47ab965bd81bb01d95e6428b63ac8dd4e80
michaelberks <michael.berks@manchester.ac.uk>
2020-12-02 17:11:39 +0000

    chore(Examples): added examples to packaging

---
+++ examples/run_madym.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+MADYM_ROOT=..
+$MADYM_ROOT/madym_T1 --config madym_T1_config.txt --cwd test_dataset
+$MADYM_ROOT/madym_AIF --config madym_AIF_config.txt --cwd test_dataset
+$MADYM_ROOT/madym_DCE --config madym_ETM_auto_config.txt --cwd test_dataset
+$MADYM_ROOT/madym_DCE --config madym_ETM_map_config.txt --cwd test_dataset
+$MADYM_ROOT/madym_DCE --config madym_ETM_pop_config.txt --cwd test_dataset
